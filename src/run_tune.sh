802ae63239e1c248161b524a921d9644c15118eb
Zhe Yu <zyu9@ncsu.edu>
2016-11-16 09:23:56 -0500

    a

--- src/run_tune.sh
+++ src/run_tune.sh
@@ -3,5 +3,5 @@ rm ./out/*
 
 for VAR in academia apple anime android scifi SE0 SE1 SE2 SE3 SE4
 do
-  bsub -q standard -W 2400 -n 10 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 10 /share2/zyu9/miniconda/bin/python2.7 tune_LDA.py exp $VAR
+  bsub -W 12000 -n 10 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 10 /share2/zyu9/miniconda/bin/python2.7 tune_LDA.py exp $VAR
 done
